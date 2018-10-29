398ec0e7aa50faa5888b5ada6aa0c98897ff9695
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2018-10-29 18:06:33 +0200

    CourtCounter

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.android.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void resetScore(View view)
+    {
+        scoreTeamA=0;
+        scoreTeamB=0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+
+    public void threePointsForA(View view)
+    {
+        scoreTeamA+=3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void twoPointsForA(View view)
+    {
+        scoreTeamA+=2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void onePointForA(View view)
+    {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    //Team B
+    public void threePointsForB(View view)
+    {
+        scoreTeamB+=3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void twoPointsForB(View view)
+    {
+        scoreTeamB+=2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void onePointForB(View view)
+    {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
